243e099a967c50a6a37ad5cb05d16bf32decdc5a
G.S. Fordham <kleine.edelweiss@outlook.com>
2021-04-28 03:57:22 -0400

    Got a basic abstraction done to implement a VolumeCtl::MasterMixer class -- this class can currently return the default (first) volume mixer channel (front-left / mono), as well as the minimum and maximum volume settings allowed by the ALSA device

--- ext/volumectl/volumectl.c
+++ ext/volumectl/volumectl.c
@@ -4,6 +4,119 @@
 #include <stdio.h>
 
 VALUE VolumeCtl = Qnil;
+VALUE MasterMixer = Qnil;
+
+// ---------------------------
+// MASTER MIXER METHODS BELOW
+// ---------------------------
+
+// Define the master mixer struct
+typedef struct {
+  snd_mixer_t* handle;
+  snd_mixer_elem_t* element;
+  int id;
+} master_mixer;
+
+// Free method for master mixer
+void master_mixer_free(void* data) {
+  free(data);
+} // End master mixer free
+
+// Size of master mixer
+size_t master_mixer_size(const void* data) {
+  return sizeof(data);
+} // End size of master mixer
+
+// Define the Ruby struct type to encapsulate the
+// master sound mixer
+static const rb_data_type_t master_mixer_type = {
+  .wrap_struct_name = "master_mixer",
+  .function = {
+    .dmark = NULL,
+    .dfree = master_mixer_free,
+    .dsize = master_mixer_size,
+  },
+  .data = NULL,
+  .flags = RUBY_TYPED_FREE_IMMEDIATELY,
+}; // End mixer handle type definition
+
+// Define allocation of mixer handle object
+VALUE master_mixer_alloc(VALUE self) {
+  // Allocate the size for a mixer handle and its opened element
+  master_mixer* mixer = malloc(sizeof(master_mixer));
+  // Wrap it up!
+  return TypedData_Wrap_Struct(self, &master_mixer_type, mixer);
+} // End mixer handle allocation
+
+// Initializer method for mixer handle
+VALUE master_mixer_m_initialize(VALUE self) {
+  snd_mixer_t *handle = NULL;
+  snd_mixer_selem_id_t *sid;
+  const char *card = "default";
+  const char *selem_name = "Master";
+  
+  snd_mixer_open(&handle, 0);
+  snd_mixer_attach(handle, card);
+  snd_mixer_selem_register(handle, NULL, NULL);
+  snd_mixer_load(handle);
+  
+  snd_mixer_selem_id_alloca(&sid);
+  snd_mixer_selem_id_set_index(sid, 0);
+  snd_mixer_selem_id_set_name(sid, selem_name);
+  
+  snd_mixer_elem_t* elem = snd_mixer_find_selem(handle, sid);
+  
+  master_mixer mm = {
+    .handle = handle,
+    .element = elem,
+    .id = -999,
+  };
+  
+  master_mixer* mixer;
+  // Unwrap
+  TypedData_Get_Struct(self, master_mixer, &master_mixer_type, mixer);
+  //mixer = (master_mixer *)malloc(sizeof(mm)); // Attach the mixer
+  *mixer = mm;
+  return self;
+} // End mix handle initializer
+
+// Get the volume of the master mixer
+VALUE method_master_mixer_volume(VALUE self) {
+  long min, max, current;
+  master_mixer* mixer;
+  // Unwrap
+  TypedData_Get_Struct(self, master_mixer, &master_mixer_type, mixer);
+  
+  snd_mixer_handle_events(mixer->handle);
+  snd_mixer_selem_get_playback_volume_range(mixer->element, &min, &max);
+  snd_mixer_selem_get_playback_volume(mixer->element, SND_MIXER_SCHN_MONO, &current);
+  
+  VALUE volumes = rb_hash_new();
+  rb_hash_aset(volumes, rb_id2sym(rb_intern("max")), LONG2NUM(max));
+  rb_hash_aset(volumes, rb_id2sym(rb_intern("min")), LONG2NUM(min));
+  rb_hash_aset(volumes, rb_id2sym(rb_intern("current")), LONG2NUM(current));
+  
+  return volumes;
+} // End volume getter for master mixer
+
+// Test MasterMixer
+VALUE method_master_mixer_test(VALUE self) {
+  master_mixer* mixer;
+  // Unwrap
+  TypedData_Get_Struct(self, master_mixer, &master_mixer_type, mixer);
+  //mixer = (master_mixer *)malloc(sizeof(mm)); // Attach the mixer
+  //snd_mixer_selem_get_playback_volume_range(self.element, &min, &max);
+  return LONG2FIX(mixer->id);
+} // End test of master mixer
+
+// Test MasterMixer set
+VALUE method_master_mixer_test_set(VALUE self, VALUE new_id) {
+  master_mixer* mixer;
+  // Unwrap
+  TypedData_Get_Struct(self, master_mixer, &master_mixer_type, mixer);
+  mixer->id = NUM2INT(new_id);
+  return LONG2FIX(mixer->id);
+} // End test of master mixer
 
 /* Get the current default sink for ALSA,
  * for use with the below methods.
@@ -72,6 +185,20 @@ VALUE method_unmute_default(VALUE self) {
   return Qnil;
 } // End dunmute
 
+// Initialize the VolumeCtl module
+void Init_volumectl() {
+  VolumeCtl = rb_define_module("VolumeCtl");
+  
+  // Master Mixer
+  MasterMixer = rb_define_class_under(VolumeCtl, "MasterMixer", rb_cData);
+  rb_define_alloc_func(MasterMixer, master_mixer_alloc);
+  rb_define_method(MasterMixer, "initialize", master_mixer_m_initialize, 0);
+  rb_define_method(MasterMixer, "volume", method_master_mixer_volume, 0);
+  rb_define_method(MasterMixer, "test", method_master_mixer_test, 0);
+  rb_define_method(MasterMixer, "test_s", method_master_mixer_test_set, 1);
+} // End init
+
+/*
 // Test for a connection
 VALUE method_test(VALUE self) {
   long min, max, volume;
@@ -114,24 +241,4 @@ VALUE method_test(VALUE self) {
   snd_mixer_close(handle);
   
   return LONG2FIX(volume);
-}
-
-// Initialize the VolumeCtl module
-void Init_volumectl() {
-  VolumeCtl = rb_define_module("VolumeCtl");
-  
-  // Get the default sink
-  rb_define_module_function(VolumeCtl, "get_dsink", method_get_default_sink, 0);
-  
-  // Set the default sink volume
-  rb_define_module_function(VolumeCtl, "get_dvolume", method_get_default_volume, 0);
-  rb_define_module_function(VolumeCtl, "set_dvolume", method_set_default_volume, -1);
-  rb_define_private_method(VolumeCtl, "pmod_dvolume", method_volume_mod, 1);
-  
-  // Mute and unmute default sink
-  rb_define_module_function(VolumeCtl, "dmute", method_mute_default, 0);
-  rb_define_module_function(VolumeCtl, "dunmute", method_unmute_default, 0);
-  
-  // Test the server
-  rb_define_module_function(VolumeCtl, "test", method_test, 0);
-} // End init
+}*/
